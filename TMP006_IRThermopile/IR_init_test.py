64d8cae22f3fff4cde5b71a7428fa12bce88eb56
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2022-02-11 00:30:22 +0000

    Library Complete: Should I work on calibration?

--- TMP006_IRThermopile/IR_init_test.py
+++ TMP006_IRThermopile/IR_init_test.py
@@ -1,23 +1,26 @@
 import IR_Library
+from time import sleep
 
 device = IR_Library.Infrared()
 
 # Test active
-print("SHOULD BE ACTIVE")
+print("Test Hypothesis: SHOULD BE ACTIVE")
 active = device.is_active() # Should be true: Device should be active upon startup
 
 # Turn off
 device.set_active(False)
-print("SHOULD BE POWERED OFF")
+print("Test Hypothesis: SHOULD BE POWERED OFF")
 active = device.is_active() # Should return false
 
 # Turn back on again
 device.set_active(True)
-print("SHOULD BE ACTIVE")
+print("Test Hypothesis: SHOULD BE ACTIVE")
 active = device.is_active()
 
 # Test ready
-# ready = device.is_ready()
+print("Test Hypothesis: SHOULD BE READY")
+ready = device.is_ready()
 
-temp = device.read_temp()
-voltage = device.read_voltage()
+device.print_config()
+
+[temp, voltage] = device.collect_reading()
