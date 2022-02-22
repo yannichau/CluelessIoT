52d80ead7e45b51aab512b0d19bb3e397e4fef69
Josiah Mendes <josiahmendes@icloud.com>
2022-02-22 23:50:48 +0000

    [App] New device flow by scanning barcode setup

--- imfresh/lib/features/home_page.dart
+++ imfresh/lib/features/home_page.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -44,7 +33,15 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         // Here we take the value from the HomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Home"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.pushNamed(context, '/addDevice');
+            },
+          ),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
--- imfresh/lib/main.dart
+++ imfresh/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
 import 'package:imfresh/features/home_page.dart';
+import 'package:imfresh/features/new_device.dart';
 import 'package:imfresh/services/databaseHandler.dart';
 
 void main() async {
@@ -22,7 +23,12 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => HomePage(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/addDevice': (context) => const NewDevice(),
+      },
     );
   }
 }
